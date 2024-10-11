08855e0342a67612d49e756bd9a86e8a47b4b585
il <widrns18@gmail.com>
2024-10-11 15:19:50 +0900

    ♻️ refactor: transform을 이용한 movieId 타입 변경

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -21,7 +21,7 @@ export class MoviesController {
   }
 
   @Get(':id')
-  getOne(@Param('id') movieId: string): Movie {
+  getOne(@Param('id') movieId: number): Movie {
     return this.moviesService.getOne(movieId);
   }
 
@@ -31,12 +31,12 @@ export class MoviesController {
   }
 
   @Delete()
-  remove(@Param('id') movieId: string) {
+  remove(@Param('id') movieId: number) {
     return this.moviesService.deleteOne(movieId);
   }
 
   @Patch(':id')
-  patch(@Param('id') movieId: string, @Body() updateData) {
+  patch(@Param('id') movieId: number, @Body() updateData) {
     return this.moviesService.update(movieId, updateData);
   }
 }
